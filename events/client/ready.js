8b64b56d70c674ff5ff8dac3c2eed9df79f60fa8
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-07-13 14:39:52 +0200

    fixing some stuff

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const config = require("../../config")
 module.exports = client => {
     console.log(`✔️  Connecté en tant que ${client.user.username} !`);
     
-    let activities = ['!help', 'avec toi'], i = 0;
+    let activities = ['!help', 'avec toi :3'], i = 0;
 
     setInterval(() => client.user.setPresence({ activity:{ name: `${activities[i++ % activities.length]}`, type: 'PLAYING'}, status: 'online'}), 5000);
 }
