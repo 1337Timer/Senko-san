19f60690859e9b37317b63e0ed6cafb0a9c49aec
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-07-14 15:35:51 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,7 @@
 module.exports = client => {
     console.log(`✔️  Connecté en tant que ${client.user.username} !`);
     
-    let activities = ['!help', `${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} membres`], i = 0;
+    let activities = ['p!help', `${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} membres`], i = 0;
 
     setInterval(() => client.user.setPresence({ activity:{ name: `${activities[i++ % activities.length]}`, type: 'WATCHING'}, status: 'online'}), 5000);
 }
