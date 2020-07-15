759ed34488f96c6204c2a5a0b3aa92c9dbc59578
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-07-15 22:57:37 +0200

    Update serverinfo.js

--- commands/Autre/serverinfo.js
+++ commands/Autre/serverinfo.js
@@ -7,13 +7,13 @@ module.exports.run = (client, message, args) => {
 
     const embed = new MessageEmbed()
     .setColor("#39A7F3")
-    .setThumbnail(guild.iconURL())
+    .setThumbnail(guild.iconURL({ format: 'png', dynamic: true}))
     .setTitle(`${guild.name}`)
     .addField(`<:Proprietaire:731207221123219548> Propriétaire :`,`${guild.owner.user}`)
     .addField(`<:Temps:731207221190328372> Créé le :`, `${moment(guild.createdAt).format('DD/MM/YYYY')}`)
     .addField(`<:Roles:731207222264332358> Roles :`, `${guild.roles.cache.map(roles => `${roles}`)}`)
     .addField(`<:Textuel:731207220611776582> Salons textuels :`, `${guild.channels.cache.filter(ch => ch.type === 'text').size} salons`)
-    .addField(`<:Vocal:731207219236044881> Salons vocaux :`, `${guild.channels.cache.filter(ch => ch.type === 'vocal').size} salons`)
+    .addField(`<:Vocal:731207219236044881> Salons vocaux :`, `${guild.channels.cache.filter(ch => ch.type === 'voice').size} salons`)
     .addField(`<:Membres:731206879732301984> Nombre de membre :`, `${guild.memberCount} membres`)
 
    message.channel.send(embed);
