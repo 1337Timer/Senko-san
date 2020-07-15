8155f770b18c6d6667eacd679aa6578f957019db
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-07-15 21:16:52 +0200

    change prefix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN: process.env.token,
-    PREFIX: "!",
+    PREFIX: "s!",
 }
--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const config = require("../../config")
 module.exports = client => {
     console.log(`✔️  Connecté en tant que ${client.user.username} !`);
     
-    let activities = ['!help', `${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} membres`], i = 0;
+    let activities = ['s!help', `${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} membres`], i = 0;
 
     setInterval(() => client.user.setPresence({ activity:{ name: `${activities[i++ % activities.length]}`, type: 'WATCHING'}, status: 'online'}), 5000);
 }
