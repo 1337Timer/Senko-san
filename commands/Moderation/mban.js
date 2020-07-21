bf9a9f238132499891be793222be1ecfa590d929
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-07-21 23:38:43 +0200

    Securing mban

--- commands/Moderation/mban.js
+++ commands/Moderation/mban.js
@@ -3,6 +3,7 @@ const { MESSAGES } = require("../../util/constants");
 
 module.exports.run = (client, message, args) => {
 
+    if (message.author.id !== "265924886461939712") return;
 
     args.forEach(async id => {
         const user = await client.users.fetch(id);
@@ -11,8 +12,9 @@ module.exports.run = (client, message, args) => {
         else message.channel.send("<:SenkoThinking:735210310138331229> Je ne peux pas ban " + id);
         
         const embed = new MessageEmbed()
+        .setAuthor("${user.username} (${user.id})")
         .setColor("#F05959")
-        .setDescription(`<:IconJoin:735207925370454066> **Action**: Multi-ban\n${user.username} (${user.id})`)
+        .setDescription(`<:IconJoin:735207925370454066> **Action**: Multi-ban`)
         .setTimestamp()
         .setFooter(message.author.username, message.author.avatarURL());
 
