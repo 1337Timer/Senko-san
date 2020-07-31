641af4c300925c8b2dc7e3455a962b4d9e36ad28
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-07-31 12:08:10 +0200

    some fixes

--- commands/Autre/activity.js
+++ commands/Autre/activity.js
@@ -4,14 +4,14 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = (client, message, args) => {
     message.guild.members.fetch().then(fetchAll => {
         const online = fetchAll.filter(m => m.presence.status === 'online');
-        const iddle = fetchAll.filter(m => m.presence.status === 'iddle');
+        const idle = fetchAll.filter(m => m.presence.status === 'idle');
         const dnd = fetchAll.filter(m => m.presence.status === 'dnd');
         const offline = fetchAll.filter(m => m.presence.status === 'offline');
     
     const embed = new MessageEmbed()
     .setColor("#39A7F3")
     .addField(`<:IconStatusOnline:735207729760829510> En ligne`, `${online.size} membres`)
-    .addField(`<:IconStatusIdle:735207706939359312> Inactif(s)`, `${iddle.size} membres`)
+    .addField(`<:IconStatusIdle:735207706939359312> Inactif(s)`, `${idle.size} membres`)
     .addField(`<:IconStatusDND:735207664140681287> Ne pas déranger`, `${dnd.size} membres`)
     .addField(`<:IconStatusOffline:735207719883112542> Invisible`, `${offline.size} membres`)
     message.channel.send(embed);
--- commands/Autre/serverinfo.js
+++ commands/Autre/serverinfo.js
@@ -12,7 +12,7 @@ module.exports.run = (client, message, args) => {
     .addField(`<:Proprietaire:735207548658909334> Propriétaire :`,`${guild.owner.user}`)
     .addField(`<:IconSlowmode:735211791554183200> Créé le :`, `${moment(guild.createdAt).format('DD/MM/YYYY')}`)
     .addField(`<:Roles:735207542946398279> Roles :`, `${guild.roles.cache.map(roles => `${roles}`)}`)
-    .addField(`<:IconTextChannel:735211943534788721>> Salons textuels :`, `${guild.channels.cache.filter(ch => ch.type === 'text').size} salons`)
+    .addField(`<:IconTextChannel:735211943534788721> Salons textuels :`, `${guild.channels.cache.filter(ch => ch.type === 'text').size} salons`)
     .addField(`<:IconVoiceChannel:735212068084514948> Salons vocaux :`, `${guild.channels.cache.filter(ch => ch.type === 'voice').size} salons`)
     .addField(`<:Membres:735207906420588645> Nombre de membre :`, `${guild.memberCount} membres`)
 
