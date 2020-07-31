49c6e2b60ada885f5834b052c25a203bf372473e
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-08-01 01:46:44 +0200

    Update messageDelete.js

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -1,22 +1,20 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client, message) => {
-    const server = client.channels.cache.get('527568704666599444')
-
-    if (server) {
-
+    const server = client.channels.cache.get('527568704666599444');
     const fetchGuildAuditLogs = await message.guild.fetchAuditLogs({
         limit: 1,
         type: 'MESSAGE_DELETE'
     });
-
     const latestMessageDeleted = fetchGuildAuditLogs.entries.first();
     const { executor } = latestMessageDeleted;
 
+    if (server) {
+        
     const embed = new MessageEmbed()
     .setAuthor("Message supprimé")
     .setColor("#DD5F53")
-    .setDescription(`<:IconJoin:735207925370454066> **Message** :\n ${message.content}\n**Auteur du message** : ${message.author}`)
+    .setDescription(`<:IconJoin:735207925370454066> **Message** :\n ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : ${message.author}`)
     .setFooter(executor.username, executor.displayAvatarURL({ format: 'png', dynamic: true}));
 
     client.channels.cache.get('699458377381838878').send(embed);
