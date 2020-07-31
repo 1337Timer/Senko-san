f95c749b4cf84195640cea456ef7b421f8c5ebd4
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-08-01 01:29:47 +0200

    Update messageDelete.js

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -1,11 +1,11 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client, message) => {
-    //const server = client.channels.cache.get('527568704666599444')
+    const server = client.channels.cache.get('527568704666599444')
 
-    //if (server) {
+    if (server) {
 
-    const fetchGuildAuditLogs = await message.guild.fetchGuildAuditLogs({
+    const fetchGuildAuditLogs = await message.guild.fetchAuditLogs({
         limit: 1,
         type: 'MESSAGE_DELETE'
     });
@@ -21,5 +21,5 @@ module.exports = async (client, message) => {
 
     client.channels.cache.get('699458377381838878').send(embed);
 
-    //}
+    }
 }
