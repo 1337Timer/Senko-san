5cc8e4d554cf368b8fa765755fd333e8131b665f
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-08-03 14:16:10 +0200

    :3

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -7,7 +7,7 @@ module.exports = async (client, message) => {
         if(message.author.bot) return;
         
     const embed = new MessageEmbed()
-    .setTitle(`Message supprimé dans ${message.channel}`)
+    .setTitle(`Message supprimé dans ${message.channel.name}`)
     .setColor("#DD5F53")
     .setDescription(`<:IconJoin:735207925370454066> **Message** :\n ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : \n${message.author}`)
     .setTimestamp()
