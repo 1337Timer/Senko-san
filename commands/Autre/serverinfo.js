4851fe400b78891646fc72dd59c77dbac654e6af
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-08-12 14:41:22 +0200

    Update serverinfo.js

--- commands/Autre/serverinfo.js
+++ commands/Autre/serverinfo.js
@@ -9,12 +9,12 @@ module.exports.run = (client, message, args) => {
     .setColor("#39A7F3")
     .setThumbnail(guild.iconURL({ format: 'png', dynamic: true}))
     .setTitle(`${guild.name}`)
-    .addField(`<:Proprietaire:735207548658909334> Propriétaire :`,`${guild.owner.user}`)
-    .addField(`<:IconSlowmode:735211791554183200> Créé le :`, `${moment(guild.createdAt).format('DD/MM/YYYY')}`)
-    .addField(`<:Roles:735207542946398279> Roles :`, `${guild.roles.cache.map(roles => `${roles}`)}`)
-    .addField(`<:IconTextChannel:735211943534788721> Salons textuels :`, `${guild.channels.cache.filter(ch => ch.type === 'text').size} salons`)
-    .addField(`<:IconVoiceChannel:735212068084514948> Salons vocaux :`, `${guild.channels.cache.filter(ch => ch.type === 'voice').size} salons`)
-    .addField(`<:Membres:735207906420588645> Nombre de membre :`, `${guild.memberCount} membres`)
+    .addField(`<:Proprietaire:735207548658909334> Propriétaire`,`${guild.owner.user}`, true)
+    .addField(`<:IconSlowmode:735211791554183200> Créé le`, `${moment(guild.createdAt).format('DD/MM/YYYY')}`, true)
+    .addField(`<:Roles:735207542946398279> Roles`, `${guild.roles.cache.size} roles`, true)
+    .addField(`<:IconTextChannel:735211943534788721> Salons textuels`, `${guild.channels.cache.filter(ch => ch.type === 'text').size} salons`, true)
+    .addField(`<:IconVoiceChannel:735212068084514948> Salons vocaux`, `${guild.channels.cache.filter(ch => ch.type === 'voice').size} salons`, true)
+    .addField(`<:Membres:735207906420588645> Nombre de membre`, `${guild.memberCount} membres`, true)
 
    message.channel.send(embed);
    message.delete();
