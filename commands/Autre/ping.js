8806e12b1002a2ba994af578ddb9cb0319f700f6
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-08-21 16:30:19 +0200

    adding useless things

--- commands/Autre/ping.js
+++ commands/Autre/ping.js
@@ -1,12 +1,13 @@
 const { MESSAGES } = require("../../util/constants");
 
 module.exports.run = async (client, message, args) => {
-    const msg = await message.channel.send("<:SenkoHappy:735207442916442153> Pong !");
+    
+const msg = await message.channel.send("Pong !");
+    
     msg.edit(
-        `
-        Latence du bot : ${msg.createdTimestamp - message.createdTimestamp}ms
-        Latence de l'API : ${Math.round(client.ws.ping)}ms
-        `
+    `
+    ⌛ Latence du bot : **${msg.createdTimestamp - message.createdTimestamp}** ms\n⏱️ Latence de l'API : **${Math.round(client.ws.ping)}** ms
+    `
     )
     
     message.delete();
--- main.js
+++ main.js
@@ -8,4 +8,24 @@ const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });;
 loadCommands(client);
 loadEvents(client);
 
+client.on('message', message => {
+
+    if (message.content.toLowerCase() === 'bonjour' || message.content.toLowerCase() === 'hey' || message.content.toLowerCase() === 'salut' || message.content.toLowerCase() === 'hello') {
+        message.react('👋');
+    }
+
+    if (message.content.toLowerCase() === 'bienvenue') {
+        message.react('❤️');
+    }
+
+    if (message.mentions.members.first()) {
+
+        let member = message.mentions.members.first();
+
+        if (member.user.id === '265924886461939712') {
+            message.react('🌺');
+        }
+    }
+})
+
 client.login(TOKEN);
