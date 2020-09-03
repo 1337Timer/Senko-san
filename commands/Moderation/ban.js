d3498aa09f8589efc71406d9bd3828c78a39ae05
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-03 16:01:39 +0200

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -3,6 +3,7 @@ const { MESSAGES } = require("../../util/constants");
 
 module.exports.run = async (client, message, args) => {
     const user = await client.users.fetch(args[0]);
+    console.log(user);
     const guild = message.guild;
     const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée.');
     user ? message.guild.member(user).ban(reason) : message.channel.send("<:SenkoThinking:735210310138331229> L'utilisateur n'existe pas.");
