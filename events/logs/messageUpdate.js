40642169e52c719848cd1f91d99ddbe8faf75d27
NYRI4 <65369563+NYRI4@users.noreply.github.com>
2020-09-06 15:15:08 +0200

    Update messageUpdate.js

--- events/logs/messageUpdate.js
+++ events/logs/messageUpdate.js
@@ -1,16 +1,14 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client, message) => {
-    const server = message.guild.id;    
-    if (server == "527568704666599444") {
-        if(message.author.bot) return;
+    
+    if(message.author.bot) return;
         
     const embed = new MessageEmbed()
-    .setTitle(`Message édité dans ${message.channel.name}`)
-    .setColor("#D29E00")
+    .setTitle(`Message supprimé dans ${message.channel.name}`)
+    .setColor("#DD5F53")
     .setDescription(`<:IconJoin:735207925370454066> **Message** :\n ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : \n${message.author}`)
     .setTimestamp()
 
     client.channels.cache.get('699458377381838878').send(embed);
-    }
 }
