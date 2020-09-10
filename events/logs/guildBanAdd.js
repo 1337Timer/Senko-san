98f3cb8a97221cfe1ea856696df59e7da4477089
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-10 20:32:20 +0200

    Update guildBanAdd.js

--- events/logs/guildBanAdd.js
+++ events/logs/guildBanAdd.js
@@ -12,3 +12,20 @@ module.exports = async (client, message) => {
 
     client.channels.cache.get('699452457104834580').send(embed);
 }
+
+const fetchedLogs = await guild.fetchAuditLogs({
+		limit: 1,
+		type: 'MEMBER_BAN_ADD',
+	});
+	const banReason = fetchedLogs.entries.first().reason;
+	const banLog = fetchedLogs.entries.first();
+
+	if (!banLog) return console.log(`${user.tag} was banned from ${guild.name} but no audit log could be found.`);
+
+	const { executor, target } = banLog;
+
+	if (target.id === user.id) {
+		message.send(`${user.tag} à été banni de ${guild.name} par ${executor.tag} car ${banReason}`);
+	} else {
+		console.log(`${user.tag} à été banni de ${guild.name}, mais on ne sait pas par qui.`);
+	}
