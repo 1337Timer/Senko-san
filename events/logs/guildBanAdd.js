f4dc03ea96d0eba282cf87cc6678f467c5b17050
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-10 20:03:46 +0200

    Create guildBanAdd.js

---
+++ events/logs/guildBanAdd.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = async (client, message) => {
+    
+    if(message.author.bot) return;
+        
+    const embed = new MessageEmbed()
+          .setAuthor(`${user.username} (${user.id})`)
+          .setColor("#F05959")
+          .setDescription(`<:IconJoin:735207925370454066> **Action**: Ban\n<:ItemMagnifyingGlass:735207739587952682> **Raison**: ${reason}`)
+          .setThumbnail(user.avatarURL())
+          .setTimestamp()
+          .setFooter(message.author.username, message.author.avatarURL({ format: 'png', dynamic: true}));
+
+    client.channels.cache.get('699452457104834580').send(embed);
+}
