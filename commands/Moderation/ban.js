b92172a8cddfe32993ac5e3534a2576c000ee64e
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-10 20:03:32 +0200

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -6,19 +6,6 @@ module.exports.run = async (client, message, args) => {
     const guild = message.guild;
     const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée.');
     user ? message.guild.member(user).ban(reason) : message.channel.send("<:SenkoThinking:735210310138331229> L'utilisateur n'existe pas.");
-
-    if (guild.id == "527568704666599444") {
-
-        const embed = new MessageEmbed()
-            .setAuthor(`${user.username} (${user.id})`)
-            .setColor("#F05959")
-            .setDescription(`<:IconJoin:735207925370454066> **Action**: Ban\n<:ItemMagnifyingGlass:735207739587952682> **Raison**: ${reason}`)
-            .setThumbnail(user.avatarURL())
-            .setTimestamp()
-            .setFooter(message.author.username, message.author.avatarURL({ format: 'png', dynamic: true}));
-
-        client.channels.cache.get('699452457104834580').send(embed);
-    }
 };
 
 module.exports.help = MESSAGES.COMMANDS.MODERATION.BAN;
