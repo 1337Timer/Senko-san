4ef7446e020537eb4858a34d6c0e947fcb21c737
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-10 20:36:35 +0200

    Update guildBanAdd.js

--- events/logs/guildBanAdd.js
+++ events/logs/guildBanAdd.js
@@ -1,17 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client, message) => {
-        
-    const embed = new MessageEmbed()
-          .setAuthor(`${user.username} (${user.id})`)
-          .setColor("#F05959")
-          .setDescription(`<:IconJoin:735207925370454066> **Action**: Ban\n<:ItemMagnifyingGlass:735207739587952682> **Raison**: ${reason}`)
-          .setThumbnail(user.avatarURL())
-          .setTimestamp()
-          .setFooter(message.author.username, message.author.avatarURL({ format: 'png', dynamic: true}));
-
-    client.channels.cache.get('699452457104834580').send(embed);
-}
 
 const fetchedLogs = await guild.fetchAuditLogs({
 		limit: 1,
@@ -25,7 +14,8 @@ const fetchedLogs = await guild.fetchAuditLogs({
 	const { executor, target } = banLog;
 
 	if (target.id === user.id) {
-		message.send(`${user.tag} à été banni de ${guild.name} par ${executor.tag} car ${banReason}`);
+		client.channels.cache.get('699452457104834580').send(`${user.tag} à été banni de ${guild.name} par ${executor.tag} car ${banReason}`);
 	} else {
-		console.log(`${user.tag} à été banni de ${guild.name}, mais on ne sait pas par qui.`);
+		client.channels.cache.get('699452457104834580').send(`${user.tag} à été banni de ${guild.name}, mais on ne sait pas par qui.`);
 	}
+}
