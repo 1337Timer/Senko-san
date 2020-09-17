63cbd48f68a0b6b1e35d5e29c0f387ebb4723807
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-17 22:17:48 +0200

    Update guildBanAdd.js

--- events/logs/guildBanAdd.js
+++ events/logs/guildBanAdd.js
@@ -1,21 +1,27 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = async (guild) => {
+module.exports = async (guild, user) => {
 
-const fetchedLogs = await guild.fetchAuditLogs({
+	const fetchedLogs = await guild.fetchAuditLogs({
 		limit: 1,
 		type: 'MEMBER_BAN_ADD',
 	});
-	const banReason = fetchedLogs.entries.first().reason;
+	// Since we only have 1 audit log entry in this collection, we can simply grab the first one
 	const banLog = fetchedLogs.entries.first();
 
+	// Let's perform a coherence check here and make sure we got *something*
 	if (!banLog) return console.log(`${user.tag} was banned from ${guild.name} but no audit log could be found.`);
 
+	// We now grab the user object of the person who banned the user
+	// Let us also grab the target of this action to double check things
 	const { executor, target } = banLog;
 
+	// And now we can update our output with a bit more information
+	// We will also run a check to make sure the log we got was for the same kicked member
 	if (target.id === user.id) {
-		client.channels.cache.get('699452457104834580').send(`${user.tag} à été banni de ${guild.name} par ${executor.tag} car ${banReason}`);
+		console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, wielded by the mighty ${executor.tag}`);
 	} else {
-		client.channels.cache.get('699452457104834580').send(`${user.tag} à été banni de ${guild.name}, mais on ne sait pas par qui.`);
+		console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, audit log fetch was inconclusive.`);
 	}
+
 }
