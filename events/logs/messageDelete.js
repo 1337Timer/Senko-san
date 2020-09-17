ebed711420ab85ffe9049b364a792fc8709f4b5e
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-17 22:07:16 +0200

    fixing stuff

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -2,15 +2,15 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = async (client, message) => {
     
-    if(message.author.bot) return;
+    if (message.author.bot) return;
 
-    console.log(message.guild.id);
-        
-    const embed = new MessageEmbed()
-    .setTitle(`Message supprimé dans ${message.channel.name}`)
-    .setColor("#DD5F53")
-    .setDescription(`<:IconJoin:735207925370454066> **Message** : ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : ${message.author}`)
-    .setTimestamp()
+    if (message.guild.id == "527568704666599444") {
+        const embed = new MessageEmbed()
+        .setTitle(`Message supprimé dans ${message.channel.name}`)
+        .setColor("#DD5F53")
+        .setDescription(`<:IconJoin:735207925370454066> **Message** : ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : ${message.author}`)
+        .setTimestamp()
 
-    client.channels.cache.get('699458377381838878').send(embed);
+        client.channels.cache.get('699458377381838878').send(embed);
+    }
 }
