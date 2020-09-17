412f972b47fdcaf90936fe49b9fb18ff970fb9e5
NYRIA <65369563+NYRI4@users.noreply.github.com>
2020-09-17 21:55:35 +0200

    test

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -7,7 +7,7 @@ module.exports = async (client, message) => {
     const embed = new MessageEmbed()
     .setTitle(`Message supprimé dans ${message.channel.name}`)
     .setColor("#DD5F53")
-    .setDescription(`<:IconJoin:735207925370454066> **Message** :\n ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : \n${message.author}`)
+    .setDescription(`<:IconJoin:735207925370454066> **Message** : ${message.content}\n**<:Membres:735207906420588645> Auteur du message** : ${message.author}`)
     .setTimestamp()
 
     client.channels.cache.get('699458377381838878').send(embed);
